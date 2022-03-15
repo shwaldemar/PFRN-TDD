d052252a2e26223a2949de288dc1a740c041a197
Simon Hall <37545481+shwaldemar@users.noreply.github.com>
2022-03-15 18:53:46 +0000

    amended migrationjs const dbMigrate equals DBMigrateDOTgetInstance

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const fastify = require('fastify');
+const db = require('../plugin/database');
 
 function build(opts = {}) {
   const app = fastify(opts);
+
+  // register plugins on application
+  app.register(db);
 
   app.get('/', async (request, reply) => {
     await reply.code(200).send({ hello: 'world!' });
