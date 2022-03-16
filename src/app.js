278db1170a3a675ede8c55eb6cb823a47f05e9e0
Simon Hall <37545481+shwaldemar@users.noreply.github.com>
2022-03-16 12:29:02 +0000

    initial commit to gh

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const fastify = require('fastify');
 const db = require('./plugin/database');
+const testRoute = require('./route/tempTestRoute');
 
 function build(opts = {}) {
   const app = fastify(opts);
 
   // register plugins
   app.register(db);
+
+  // register route as/like a plugin
+  app.register(testRoute, { prefix: 'api/v1/test' });
 
   app.get('/', async (request, reply) => {
     await reply.code(200).send({ hello: 'world!' });
